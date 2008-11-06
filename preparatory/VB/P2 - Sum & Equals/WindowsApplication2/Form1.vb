1e544ad94cb0fc43701531facd7eb4b0baabb80d
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2008-11-06 08:44:15 +0200

    Add functionality for clearing the input field
    The current commit adds a feature to clear the input field when the user clicks on the Clear button. The Clear button triggers the Button1_Click event, which sets the value of the input field to an empty string. This will allow users to start fresh when performing calculations.

---
+++ preparatory/VB/P2 - Sum & Equals/WindowsApplication2/Form1.vb
@@ -0,0 +1,15 @@
+Public Class Form1
+    Dim x As Integer
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim y As Integer
+        y = Convert.ToInt32(TextBox1.Text)
+        x = x + y
+        TextBox1.Text = x.ToString
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        x = Convert.ToInt32(TextBox1.Text)
+        TextBox1.Text = " "
+    End Sub
+End Class
